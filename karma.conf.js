b480a86c77b6c7d7f797867739d5c227c5804f36
Trevor Allred <tallred@picsauditing.com>
2014-03-14 21:02:51 -0700

    removed unneeded dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -1,16 +1,15 @@
-module.exports = function(config) {
+module.exports = function (config) {
     config.set({
-        frameworks: ['jasmine'],
         basePath: './',
         files: [
             'main.js',
             'main.spec.js'
         ],
- 
-        // test results reporter to use
+
         reporters: ['dots'],
- 
+
         autoWatch: true,
+        frameworks: ['jasmine'],
         browsers: ['PhantomJS'],
         captureTimeout: 10000,
         singleRun: false
